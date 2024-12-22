0f70c60947b0ed3cc6676493455255912529c846
foldcat <151979723+foldcat@users.noreply.github.com>
2024-12-22 14:19:34 +0800

    lsp keymap

--- fnl/plugins/lspconfig.fnl
+++ fnl/plugins/lspconfig.fnl
@@ -15,4 +15,20 @@
     :config 
     (fn []
       (vim.tbl_map (. (require :astrolsp) :lsp_setup)
-      (. (require :astrolsp) :config :servers)))}]	
+      (. (require :astrolsp) :config :servers))
+
+      (local map vim.keymap.set)
+
+      (map :n :gD vim.lsp.buf.declaration)
+      (map :n :gd vim.lsp.buf.definition)
+      (map :n :gi vim.lsp.buf.implementation)
+      (map :n :<leader>sh vim.lsp.buf.signature_help)
+      (map :n :<leader>wa vim.lsp.buf.add_workspace_folder)
+      (map :n :<leader>wr vim.lsp.buf.remove_workspace_folder)
+      (map :n :<leader>wl
+           (fn []
+             (print (vim.inspect (vim.lsp.buf.list_workspace_folders)))))
+      (map :n :<leader>D vim.lsp.buf.type_definition)
+      (map :n :<leader>ra vim.lsp.buf.rename)
+      (map [:n :v] :<leader>ca vim.lsp.buf.code_action)
+      (map :n :gr vim.lsp.buf.references))}]	
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,6 +6,21 @@ local function _1_()
   return {handlers = {_2_}}
 end
 local function _3_()
-  return vim.tbl_map(require("astrolsp").lsp_setup, require("astrolsp").config.servers)
+  vim.tbl_map(require("astrolsp").lsp_setup, require("astrolsp").config.servers)
+  local map = vim.keymap.set
+  map("n", "gD", vim.lsp.buf.declaration)
+  map("n", "gd", vim.lsp.buf.definition)
+  map("n", "gi", vim.lsp.buf.implementation)
+  map("n", "<leader>sh", vim.lsp.buf.signature_help)
+  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder)
+  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder)
+  local function _4_()
+    return print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end
+  map("n", "<leader>wl", _4_)
+  map("n", "<leader>D", vim.lsp.buf.type_definition)
+  map("n", "<leader>ra", vim.lsp.buf.rename)
+  map({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action)
+  return map("n", "gr", vim.lsp.buf.references)
 end
 return {{"williamboman/mason.nvim", opts = {}}, {"neovim/nvim-lspconfig", event = {"BufRead", "VeryLazy"}, dependencies = {{"AstroNvim/astrolsp", opts = {}}, {"williamboman/mason-lspconfig.nvim", dependencies = "williamboman/mason.nvim", opts = _1_}}, config = _3_}}
