745bf63eb801a6d27c959c150791d6924168263f
foldcat <151979723+foldcat@users.noreply.github.com>
2025-01-30 13:59:59 +0800

    a lot of new colorschemes

--- fnl/plugins/colorscheme.fnl
+++ fnl/plugins/colorscheme.fnl
@@ -1,9 +1,17 @@
 [{1 :nyoom-engineering/oxocarbon.nvim
     :lazy false
-    :priority 1000
+    :priority 1000} 
+
+ {1 :dgox16/oldworld.nvim
+    :priority 1000 
+    :lazy false}
+
+ {1 :drewxs/ash.nvim
+    :lazy false 
+    :priority 1000 
     :config 
     (fn []
-       (vim.cmd "colorscheme oxocarbon"))} 
+      (vim.cmd.colorscheme :ash))}
 
  {1 :jackplus-xyz/binary.nvim
     :lazy false 
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 -- [nfnl] Compiled from fnl/plugins/colorscheme.fnl by https://github.com/Olical/nfnl, do not edit.
 local function _1_()
-  return vim.cmd("colorscheme oxocarbon")
+  return vim.cmd.colorscheme("ash")
 end
-return {{"nyoom-engineering/oxocarbon.nvim", priority = 1000, config = _1_, lazy = false}, {"jackplus-xyz/binary.nvim", priority = 1000, opts = {style = "dark"}, lazy = false}}
+return {{"nyoom-engineering/oxocarbon.nvim", priority = 1000, lazy = false}, {"dgox16/oldworld.nvim", priority = 1000, lazy = false}, {"drewxs/ash.nvim", priority = 1000, config = _1_, lazy = false}, {"jackplus-xyz/binary.nvim", priority = 1000, opts = {style = "dark"}, lazy = false}}
